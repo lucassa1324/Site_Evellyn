0520bc6b6503cff5e1eb328022b15395536d095b
Lucas Sá <lucassa1324@gmail.com>
2024-12-10 08:53:00 -0300

    Site V 1.3 agendamentos e caminhos

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+// next.config.mjs
+import { defineConfig } from 'next';
+
+export default defineConfig({
+  reactStrictMode: true,
+  async redirects() {
+    return [
+    
+    {
+        source: '/',
+        destination: '/index.html', // Redireciona para o index.html na raiz
+        permanent: false,
+
+    },   
+    {
+        source: '/agendamento', // URL que você deseja acessar
+        destination: '/agendamento.html', // URL do arquivo HTML
+        permanent: false,
+      },
+    ];
+  },
+});
