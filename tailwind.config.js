ea559eb246c0922297cd278d12789f5b4c62cae4
Lucas Sá <lucassa1324@gmail.com>
2024-12-17 08:58:33 -0300

    Corrigindo caminhos no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.html"],
+module.exports = {
+  content: [
+    './index.html',          // Se o index.html estiver na raiz
+    './public/**/*.html',    // Para outros HTML na pasta public
+    './src/**/*.{js,ts}',    // Para arquivos JavaScript ou TypeScript no src
+  ],
   theme: {
     extend: {},
   },
